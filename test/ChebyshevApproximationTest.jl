c6acaf3b1f994f51a906ffdb144c9ad76f149b33
leon <olifer@ualberta.ca>
2019-03-27 16:50:02 -0600

    New test

--- test/ChebyshevApproximationTest.jl
+++ test/ChebyshevApproximationTest.jl
@@ -2,23 +2,23 @@ using Plots
 using Main.ChebyshevApproximation
 
 function f(x)
-    return exp.(x)
+    return exp.(-x.^2/0.1)
 end
 
 xFunc = -1:0.001:1
 yFunc = f(xFunc)
 plot(xFunc, yFunc)
 
-@time Cns, C0 = GetCnList(f)
-a = 0.123456789
-b = 0.56789101112
-int = ChebyshevApproximation.ChebyshevFitDefinedIntegral(a, b, Cns, C0)
+@time Cns, C0 = GetCnList(f, 40)
+a = [-1., -0.95, -0.96]
+b = [0.1, 0.2, 0.3]
+int = ChebyshevFitDefinedIntegral(a, b, Cns, C0)
 
-# fit = ChebyshevFit(xFunc, Cns, C0)
+fit = ChebyshevFit(xFunc, Cns, C0)
 
 # fit = ChebyshevFitIntegral(0.123456789, Cns, C0) - ChebyshevFitIntegral(-0.123456789, Cns, C0)
 
 
 
-# plot!(xFunc, fit)
+plot!(xFunc, fit)
 # plot(xFunc, fit .- yFunc .- 1)
